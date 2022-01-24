4db7a18f6974711c9a8bb8744ca8da1a9949c004
Torsten Baldes <t.baldes@medienfreunde.com>
2022-01-24 11:20:20 +0100

    Use GTAG events

--- analytics-file-downloads.js
+++ analytics-file-downloads.js
@@ -1,5 +1,5 @@
 /**
- * jQuery GA Track Downloads
+ * jQuery GTAG Track Downloads
  *
  * This script will allow Google Analytics to track when a user downloads file attachements
  *
@@ -31,24 +31,34 @@ $(document).ready(function($) {
         elEv.loc = baseHref + href;
       }
       else if (href.match(/^https?\:/i) && !isThisDomain) {
-        elEv.category = "external";
+        elEv.category = "extern";
         elEv.action = "click";
         elEv.label = href.replace(/^https?\:\/\//i, '');
         elEv.non_i = true;
         elEv.loc = href;
       }
       else if (href.match(/^tel\:/i)) {
-        elEv.category = "telephone";
+        elEv.category = "telefon";
         elEv.action = "click";
         elEv.label = href.replace(/^tel\:/i, '');
         elEv.loc = href;
       }
       else track = false;
 
-     	if (track) {
-        ga('send', 'event', elEv.category.toLowerCase(), elEv.action.toLowerCase(), elEv.label.toLowerCase(), elEv.value, elEv.non_i);
+     	if (track && typeof gtag == 'function') {
+        gtag('event', elEv.action.toLowerCase(), {
+          'event_category': elEv.category.toLowerCase(),
+          'event_label': elEv.label.toLowerCase(),
+          'value': elEv.value,
+          'non_interaction': elEv.non_i,
+          'transport_type': 'beacon',
+          'event_callback': function(){
+            if ( el.attr('target') == undefined || el.attr('target').toLowerCase() != '_blank') {
+              location.href = elEv.loc; };
+            }
+           }
+        });
         if ( el.attr('target') == undefined || el.attr('target').toLowerCase() != '_blank') {
-          setTimeout(function() { location.href = elEv.loc; }, 400);
           return false;
         }
       }
